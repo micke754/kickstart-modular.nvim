a4be0fa0a78dfae19d1528b7df10f63df6a34eae
Kevin Michael <micke51p@registry.otago.ac.nz>
2024-06-05 15:55:08 +1200

    Added Neotree Plugin + Keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>e', ':Neotree toggle filesystem right<CR>', {})
+      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+    end,
+  },
+}
