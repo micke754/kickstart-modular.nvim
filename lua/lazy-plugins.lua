570984daef382d66cea280b6202c971db56e5826
Kevin Michael <micke51p@registry.otago.ac.nz>
2024-06-11 11:07:04 +1200

    Adding Noice to give us a nicer commandline interface, thanks Folke!!!

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -47,7 +47,8 @@ require('lazy').setup({
   require 'custom/plugins/neo-tree',
   require 'custom.plugins.nvim-r',
   require 'custom.plugins.none-ls',
-  require 'custom.plugins.dadbod'
+  require 'custom.plugins.dadbod',
+  require 'custom.plugins.noice'
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
