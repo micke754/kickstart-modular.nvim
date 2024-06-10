8e85c9d58beedc3426d0022742d948b310455ea8
Kevin Michael <micke51p@registry.otago.ac.nz>
2024-06-11 11:13:53 +1200

    Keymap for Quicker Commandline, inspired by NVChad.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Easier Commandline 
+vim.keymap.set('n', ';', ':', { desc = 'Quicker Commandline'})
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
