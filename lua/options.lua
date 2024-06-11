bd2faacd29b6c89564c2cd49c4c590a646e8d732
Kevin Michael <micke51p@registry.otago.ac.nz>
2024-06-11 17:12:21 +1200

    Had forgotten to enable TJ's terminal QoL improvements

--- lua/options.lua
+++ lua/options.lua
@@ -59,6 +59,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+--NOTE: My personal options
 require 'custom/options/neovide-options'
+require 'custom.options.personal-options'
+require 'custom.options.terminal'
 
 -- vim: ts=2 sts=2 sw=2 et
