1636700eb8fa28c06de27177c36a21bc50aa1d15
Kevin Michael <micke51p@UO115076.registry.otago.ac.nz>
2024-07-09 16:49:50 +1200

    tweaking gruvbox

--- lua/custom/themes/gruvbox.lua
+++ lua/custom/themes/gruvbox.lua
@@ -5,7 +5,9 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_italic = false
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_foreground = 'mix'
     vim.cmd.colorscheme 'gruvbox-material'
   end,
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,10 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc',
+        'nushell', 'nix'
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
